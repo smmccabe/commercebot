022da9dff00edb516e9a49b2818e5b2eb5841f0a
Shawn McCabe <smccabe@acromediainc.com>
2019-01-26 00:27:21 +0000

    Add CI Support

--- test/CommerceBotTest.js
+++ test/CommerceBotTest.js
@@ -4,13 +4,13 @@ var CommerceBot = require('../src/CommerceBot');
 describe('CommerceBot', () => {
     describe('checkout()', () => {
         it('Should complete checkout - Commerce Plus', async () => {
-            let bot = new CommerceBot();
+            let bot = new CommerceBot(true);
             const result = await bot.checkout('https://commerceplus.acromedia.com');
             result.should.equal('Checkout Completed');
             await bot.close();
         }).timeout(60000);
         it('Should reach step 7 - USI', async () => {
-            let bot = new CommerceBot();
+            let bot = new CommerceBot(true);
             const result = await bot.checkout('https://www.usi-laminate.com');
             result.should.equal('Checkout Incomplete, failed at step 7');
             await bot.close();
@@ -18,7 +18,7 @@ describe('CommerceBot', () => {
     });
     describe('faq()', () => {
         it('Should not find an FAQ', async () => {
-            let bot = new CommerceBot();
+            let bot = new CommerceBot(true);
             const result = await bot.faq('https://commerceplus.acromedia.com');
             result.should.not.equal('FAQ Found');
             await bot.close();
@@ -26,7 +26,7 @@ describe('CommerceBot', () => {
     });
     describe('newsletter()', () => {
         it('Should find a newsletter signup', async () => {
-            let bot = new CommerceBot();
+            let bot = new CommerceBot(true);
             const result = await bot.newsletter('https://commerceplus.acromedia.com');
             result.should.equal('Newsletter signup found');
             await bot.close();
